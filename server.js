d9d73df768cedb44dcb2be5e11dcfe3c4a83e25e
Nicolas Terray <nicolas.terray@gmail.com>
2014-06-02 23:14:22 +0200

    Generate (lot of) dummy data

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 var express = require('express'),
-    app = express();
+    app = express()
+    lorem = require('lorem-ipsum')
+    NB_OF_SPRINTS = 100,
+    NB_OF_STORIES = 1000;
+
+function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+}
+
+app.get('/api/plan.json', function(req, res) {
+    var lorem_settings = {count: 3, units: 'words'}
+        , plan = [];
+
+    for (var i = 0 ; i < NB_OF_SPRINTS ; i++) {
+        plan.push({title: lorem(lorem_settings)});
+    }
+    res.send(plan);
+});
+
+app.get('/api/backlog.json', function(req, res) {
+    var lorem_settings = {count: 1, units: 'sentences'}
+        , backlog = [];
+
+    for (var i = 0 ; i < NB_OF_STORIES ; i++) {
+        backlog.push({
+            xref: 'story #'+ i,
+            points: getRandomArbitrary(1, 21),
+            color: 'blue',
+            title: lorem(lorem_settings)
+        });
+    }
+    res.send(backlog);
+});
 
 app.use(express.static('./public'));
 
